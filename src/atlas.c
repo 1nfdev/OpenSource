a242596bb0d009539c5c0c05a64cdfc1bd84e97a
infdev <54375346+1nfdev@users.noreply.github.com>
2020-02-26 00:53:11 +0500

    Update atlas.c

--- src/atlas.c
+++ src/atlas.c
@@ -1,3 +1,7 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
 #include "atlas.h"
 
 struct AtlasRect { unsigned int x, y, w, h; };
